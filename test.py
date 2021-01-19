be8d900bf6b0f6e731a03b9be67faed83adf4de8
Tanmay Dureja <tanmaydureja@gmail.com>
2021-01-19 16:55:05 -0500

    Event Summary Test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import json
+import requests
+r = requests.get('https://api.github.com/networks/vuetifyjs/vuetify/events')
+x = r.json()
+print (x)
+# with open('test.json') as f:
+#   data = json.load(f)
 
-with open('test.json') as f:
-  data = json.load(f)
+# print(json.dumps(data, indent = 4, sort_keys=True))
 
-print(json.dumps(data, indent = 4, sort_keys=True))
-
-print (data[0]["actor"]['login'])
+# print (data[0]["actor"]['login'])
